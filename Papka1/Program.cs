f9c5d16ad1f73dd1f0e46affa58a68761000448e
Viktor Belous <vyntyk@yandex.ru>
2022-08-11 22:23:24 +0300

    Задача 21 и 23

---
+++ Papka1/Program.cs
@@ -0,0 +1,22 @@
+﻿/*Задача 23
+Напишите программу, которая принимает
+ на вход число (N) и выдаёт таблицу 
+кубов чисел от 1 до N.
+3 -> 1, 8, 27
+5 -> 1, 8, 27, 64, 125
+*/
+Console.WriteLine("Inter number");
+int N = Convert.ToInt32(Console.ReadLine());
+
+for (int i = 1; i <= N; i++)
+{
+    int result = i * i * i;
+    if (i == N)
+    {
+        Console.Write(result);
+    }
+    else
+    {
+        Console.Write(result + ", ");
+    }
+}
---
+++ Papka2/Program.cs
@@ -0,0 +1,29 @@
+﻿/*Задача 21
+Напишите программу, которая принимает на вход
+ координаты двух точек и находит расстояние 
+ между ними в 3D пространстве.
+ A (3,6,8); B (2,1,-7), -> 15.84
+A (7,-5, 0); B (1,-1,9) -> 11.53
+*/
+
+Console.WriteLine("Введите х1:");
+int x1 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите y1:");
+int y1 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите z1:");
+int z1 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите x2:");
+int x2 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите y2:");
+int y2 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите z2:");
+int z2 = Convert.ToInt32(Console.ReadLine());
+
+double result = Math.Sqrt((x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1) + (z2 - z1) * (z2 - z1));
+result = Math.Round(result, 2);
+Console.WriteLine(result);
